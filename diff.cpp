4c711c17628cadd3d1c965729306179afdaadbb4
krishnapal Singh <ks615304@gmail.com>
2023-05-13 03:18:28 +0530

    added implimantatyion in diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     int a=40;
     int b=5;
-    cout<<"the diff is "<<(a-b);
+    cout<<"the diff is "<<(a-b)<<endl;
+    cout<<" the given answer is correct"<<endl;
 }
